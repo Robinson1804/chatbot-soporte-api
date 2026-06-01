2d863cc473203865fdf0b0188cac55c774764326
Robinson1804 <gptpanas@gmail.com>
2026-06-01 10:10:52 -0500

    feat: agregar middleware de validacion de red interna para /api/ticket-ssi

---
+++ middleware/internalOnly.js
@@ -0,0 +1,19 @@
+const ipRangeCheck = require('ip-range-check');
+
+function internalOnly(req, res, next) {
+  if (process.env.SKIP_IP_VALIDATION === 'true') return next();
+
+  const range = process.env.INTERNAL_IP_RANGE || '10.0.0.0/8';
+  const ip = req.ip || req.connection.remoteAddress || '';
+  const cleanIp = ip.replace(/^::ffff:/, '');
+
+  if (cleanIp === '127.0.0.1' || cleanIp === '::1') return next();
+
+  if (!ipRangeCheck(cleanIp, range)) {
+    return res.status(403).json({ error: 'Acceso restringido a la red interna INEI.' });
+  }
+
+  next();
+}
+
+module.exports = internalOnly;
